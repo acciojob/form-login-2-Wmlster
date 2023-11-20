b5e0f918e1ecaf498907e96f3cdde50e4d9f09fa
Manik Sharma <90952302+Wmlster@users.noreply.github.com>
2023-11-20 18:32:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+function FormDetail(){
+	let FirstName=document.getElementById("fname");
+
+	let LastName=document.getElementById("lname");
+
+	let PhoneNo=document.getElementById("phone");
+
+	let MailId=document.getElementById("mailid");
+	
+	//alert(FirstName,LastName);
+
+	var f = document.getElementById('myForm').getElementsByTagName("INPUT");
+
+for (var i = 0; i < f.length; i++)
+    alert(f[i].name + ' ' + f[i].value);
+	
+}
